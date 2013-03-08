b6bac239794edff9ea8ceb2c4b10b72e88b12976
chenwenli <kapa2robert@gmail.com>
2013-03-08 22:14:41 +0800

    20130308.1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 var App;
 function application(){		
+	var _cache = function(){		
 	
-	var _cache = function(){
-		
 	};
 	
 	var _init = function(){
@@ -16,13 +15,14 @@ function application(){
 //function layout
 App.prototype = new application();
 
-function  appPanel(){
-	var _panel = {}
-	var _page  = { 
-		curid : '',
-		preid : ''
-	}
-	this.go = function(id, callback){
+App.Widget.Panel = funciton(){
+		var _panel = {}
+		var _page  = { 
+			curid : '',
+			preid : ''
+		};
+
+	this.Open=  function(id, callback){
 		if (id  == _page.curid ){
 			console.log('already stay at this');
 			return false;
@@ -38,9 +38,9 @@ function  appPanel(){
 		var curobj = _panel[id];
 
 		if( curobj.loaded == 0 || updateflag){//panel not  load or per switch update content
-			uexWindow.openPopover(id,"0",url,"",int(x),int(y),int(s.width),int(s.height),int(s.fontSize),"0");
-			App.Controller.run;
+			callback();
 		}
+	
 		_page.preid = _page.curid;
 		_page.curid = id;
 		// doing switch window;
@@ -49,8 +49,9 @@ function  appPanel(){
 		p1.style.display = "block !important";
 		p2.style.display ='none !important';
 		
-	};
-}; 
+	}
+
+	}
 
 
 
@@ -59,13 +60,14 @@ function _event(cb,p){
 	var srcobj = $(srcele);
 	var url = srcobj.attr('data-url');
 	var name = srcobj.attr('data-title');
-	var name = srcobj.attr('data-bind');
+	var bindname = srcobj.attr('data-bind');
 	var onlyid = "";
 	//表示切换 一个html的多个panel
 	if(url.indexOf('#') == 0){
 		var _label = url.substr(1);
-		var _pInstance = new appPanel();
-		_pInstance.go(_label, cb);
+		// App.panel.go()
+		//statrt read binded data
+		App.Widget.Panel.Open(_label,  App.Controller.done(bindname) );
 
 	}else if (url.indexOf('.html') != -1){
 		//表示content的切换
@@ -77,19 +79,43 @@ function _event(cb,p){
 
 }	
 
-var ctx = App.Route({
-	first : 'IndexController',
-	second : 'SecondController',
-	third : 'ThirdController'
-});
+// var ctx = App.Route({
+// });
 
-
-App.init = function(){
+App.ready= function(){
 
 }
 
-//html code: linkto index.html
-App.Controller.IndexController = function(){};
-App.Controller.SecondController = function(){};
+App.Controller.prototype = function(){
+	var _c = {};
+	this.reg = function(control, preparecb){
+		control = control.toLowerCase();
+		if(!_c[control]){
+			_c[control] = {};
+			preparecb(_c[control]);	
+		}		
+		return  _c[control];
+	}
+	this.run = function(bindname){
+		var list = bindname.split('/');
+		var c = s[0].toLowerCase();
+		var a = s[1];
+		if(!_c[c]){
+			console.log('Error::Not found Controll Name as '+ c);
+		}else if(!_c[c][a+'Action']){
+			console.log('Error::Not found Action Name as '+ a);
+		}else{
+			return _c[c][a];
+		}
 
-App.Controller.ThirdController =function(){};
+	}
+}
+
+//html code: linkto index.html
+var newControl = App.Controller.reg('News','');
+newControl.hotAction = function(){}
+newControl.digAction = function(page,updateflag){
+	$.getJson('');
+}
+
+App.View.newsdig = function(){}
--- js/debug.js
+++ js/debug.js
@@ -1,14 +1,18 @@
 //debug file
 var _debugconfig = {
 	host:"192.168.1.151",
-	port:8081,
+	port:':8081',
 	user:'cwl'
 	scriptsrc :"/target/target-script-min.js#"
 }
 function require(debugflag){
 	 if(debugflag =='debug'){
 	 	 var s = document.createElement('script');
-	 	 s.setAttribute()
+	 	 var full = 'http://'+host +port +scriptsrc+user;
+	 	 s.setAttribute('src',full);
+	 	 s.setAttribute('type','text/javascript');
+	 	 document.getElementsByTagName('head').appendChild(s);
+	 	 return false;
 	 }
 	 if(debugflag =='nocache'){
 
